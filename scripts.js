863c8824f819350ce9f44f37d1518213404ed924
Carlos Moncada Garcia <carlos.garcia@west-mec.org>
2015-12-15 18:10:42 -0700

    Final Project

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by session2 on 10/20/15.
+ */
+$(document).ready(function(){
+    $('ImageDivName').click(function(){
+        $("ImageDivName").attr('src',"ImageSR");
+
+    });
+});
+
+
+
